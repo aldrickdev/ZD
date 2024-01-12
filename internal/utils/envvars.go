8a8b2270eb197b1010a70b6e14ead87274f70899
aldrick.castro <aldrick.castro@datadoghq.com>
2024-01-11 21:02:00 -0500

    fixing utils

--- internal/utils/envvars.go
+++ internal/utils/envvars.go
@@ -18,17 +18,10 @@ var Env envVars
 
 func LoadEnvVars() {
 	Env.ENV = os.Getenv("ENV")
-	// Env.USER_SRV_DOMAIN = os.Getenv("USER_SRV_DOMAIN")
-	// Env.USER_SRV_PORT = os.Getenv("USER_SRV_PORT")
-	// Env.RMQ_USER = os.Getenv("RMQ_USER")
-	// Env.RMQ_PASS = os.Getenv("RMQ_PASS")
-	// Env.RMQ_DOMAIN = os.Getenv("RMQ_DOMAIN")
-	// Env.RMQ_PORT = os.Getenv("RMQ_PORT")
-
-	Env.USER_SRV_DOMAIN = "localhost"
-	Env.USER_SRV_PORT = "3000"
-	Env.RMQ_USER = "guest"
-	Env.RMQ_PASS = "guest"
-	Env.RMQ_DOMAIN = "localhost"
-	Env.RMQ_PORT = "5672"
+	Env.USER_SRV_DOMAIN = os.Getenv("USER_SRV_DOMAIN")
+	Env.USER_SRV_PORT = os.Getenv("USER_SRV_PORT")
+	Env.RMQ_USER = os.Getenv("RMQ_USER")
+	Env.RMQ_PASS = os.Getenv("RMQ_PASS")
+	Env.RMQ_DOMAIN = os.Getenv("RMQ_DOMAIN")
+	Env.RMQ_PORT = os.Getenv("RMQ_PORT")
 }

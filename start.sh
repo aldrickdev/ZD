11056d9a2685bce38a3d480100d955dcd6068cb0
aldrick.castro <aldrick.castro@datadoghq.com>
2024-02-28 14:17:13 -0500

    Updated start.sh script

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 GO_VERSION=1.21.6
 
-goenv install $GO_VERSION
+/home/ubuntu/.goenv/bin/goenv install $GO_VERSION
 
 /home/ubuntu/.goenv/versions/$GO_VERSION/bin/go build -o build/zd cmd/zd/main.go
 ./build/zd

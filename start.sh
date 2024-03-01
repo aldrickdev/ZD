52e01a2bc5bd4815d46068adf80a0aa91b6bbcae
aldrick.castro <aldrick.castro@datadoghq.com>
2024-03-01 16:06:26 -0500

    Update start.sh to use asdf

--- start.sh
+++ start.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+# Get runtime version
 VERSION=$(cat version)
-export $(cat env)
 
-/home/ubuntu/.goenv/bin/goenv install $VERSION
+# Setup asdf variables
+ASDF_BIN_DIR="/home/ubuntu/.asdf/bin"
+ASDF_STARTUP_SCRIPT="/home/ubuntu/.asdf/asdf.sh"
 
-/home/ubuntu/.goenv/versions/$VERSION/bin/go build -o build/zd cmd/zd/main.go
+# Load asdf
+source $ASDF_STARTUP_SCRIPT
+
+# Install the proper runtime version
+$ASDF_BIN_DIR/asdf install golang $VERSION
+$ASDF_BIN_DIR/asdf local golang $VERSION
+
+# Load changes
+source $ASDF_STARTUP_SCRIPT
+
+# Build executable and run it
+$ASDF_BIN_DIR/asdf exec go build -o build/zd cmd/zd/main.go
 ./build/zd
